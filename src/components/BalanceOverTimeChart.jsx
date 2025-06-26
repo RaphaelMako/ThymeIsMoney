e8026c79a84e31094faf0fd30fe7b315a86d525b
Raphael Mako <makoto.sorensen@gmail.com>
2025-06-25 22:02:37 -0400

    more play

--- src/components/BalanceOverTimeChart.jsx
+++ src/components/BalanceOverTimeChart.jsx
@@ -143,12 +143,12 @@ function BalanceOverTimeChart({ data }) {
     <ResponsiveContainer width="100%" height={400}>
       <AreaChart data={data3} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
         <defs>
-          <linearGradient id="pv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="60%" stopColor="#8884d8" stopOpacity={1} />
+            <stop offset="100%" stopColor="#ffffff" stopOpacity={1} />
           </linearGradient>
         </defs>
-        {/* <Area type="monotone" dataKey="pv" fill="#8884d8" activeDot={{ r: 8 }} /> */}
+        <Area type="monotone" dataKey="pv" fill="url(#colorPv)" strokeWidth={10} stroke="black" activeDot={{ r: 8 }} />
       </AreaChart>
     </ResponsiveContainer>
   );
